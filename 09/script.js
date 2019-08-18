0631087b708755ffc26e35e7f23f0987425b2a1c
kirandash <thekirankumardash@gmail.com>
2019-08-18 18:23:45 +0800

    Q9 Repeated characters in a string

---
+++ 09/script.js
@@ -0,0 +1,12 @@
+(function(){
+    function repeatation(char, string){
+        // Repeated characters in a string
+        let input = string.toString().toLowerCase().split("");
+        let count = 0;
+        for(i=0; i<input.length; i++){
+            if(input[i] === char.toLowerCase()) count++;
+        }
+        return count;
+    }
+    console.log(repeatation('S', 'Salsas!'));
+})();
