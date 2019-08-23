1d86654745eb42a11894ddb6dc2b2b64d2e0352a
kirandash <thekirankumardash@gmail.com>
2019-08-23 23:49:43 +0800

    16. Inheritance with objects using a1.call(this, x)

---
+++ 16/script.js
@@ -0,0 +1,36 @@
+(function(){
+    const a = function(x){
+        // Constructor or Super
+        this.x = x;
+    }
+    const b = function(y){
+        this.y = y;
+    }
+    const newB = new b('x','y');
+    // newB.getX();
+    // newB.getY(); // Use inheritance so that these returns x and y from b function
+
+    // Answer
+    const a1 = function(x){
+        // Constructor or Super
+        this.x = x;
+        this.getX = function(){
+            return this.x;
+        }
+    }
+
+    const b1 = function(x,y){
+        this.y = y;
+        a1.call(this, x); // Executes a1 function and inherits everything from a1 and makes it available via this to b1
+        this.getY = function(){
+            return this.y;
+        }
+    }
+
+    const newA1 = new a1('straight x');
+    console.log(newA1.getX());
+
+    const newB1 = new b1('inherited x',' straight y');
+    console.log(newB1.getX());
+    console.log(newB1.getY());
+})();
